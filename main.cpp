2cc2feed2d25289172741201c93401b1a80971d1
tuttuwucha <tuttuwucha@gmail.com>
2026-08-14 18:00:40 +1000

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,27 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/Color.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Angle.hpp>
 #include <SFML/Window/Event.hpp>
 #include <SFML/Window/Keyboard.hpp>
 #include <SFML/Window/VideoMode.hpp>
 
 int main() {
-	// В SFML 3 размеры передаются как единый вектор sf::Vector2u
-	int width = 640;
-	int height = 360;
-	sf::RenderWindow * window = new sf::RenderWindow(sf::VideoMode({width, height}), "Title, nigga");
-	window->setFramerateLimit(60);
+	unsigned  width = 640;
+	unsigned  height = 360;
+	sf::RenderWindow * window = new sf::RenderWindow(sf::VideoMode({width, height}), "Title");
+	window->setFramerateLimit(100);
+
+	sf::CircleShape circle(67.0f);
+	circle.setOrigin(circle.getGeometricCenter());
+	circle.setPosition({width / 2.0f, height / 2.0f});
+	circle.setFillColor(sf::Color::Green);
+	circle.setOutlineThickness(3.0f);
+	circle.setOutlineColor(sf::Color::White);
+	circle.setPointCount(3);
+
+
 
 	while(window->isOpen()){
 
@@ -25,10 +37,14 @@ int main() {
 				}
 			}
 		}
+		circle.rotate(sf::radians(1 / 67.0f));
+		circle.move({1.0f, -2.0f});
 		//render
-		window->clear(sf::Color(0xFF9900FF));
+		window->clear();
 
 		//drawing
+		window -> draw(circle);
+
 
 		window->display();
 	}
